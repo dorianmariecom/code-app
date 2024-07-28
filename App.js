06dff3f6c63960bd7ccb9b81edfa273c13afd773
Dorian Marié <dorian@dorianmarie.com>
2024-07-28 20:42:39 +0200

    try to get the app running in 4 envs

--- App.js
+++ App.js
@@ -2,7 +2,14 @@ import React from 'react';
 import {Text, View} from 'react-native';
 import Config from 'react-native-config';
 
-const CODE_ENV = Config.CODE_ENV;
+const CODE_ENV = Config.CODE_ENV || "production";
+const URLS = {
+  local: "http://localhost:3000",
+  dev: "https://dev.codedorian.com",
+  staging: "https://staging.codedorian.com",
+  production: "https://codedorian.com"
+};
+const CODE_URL = URLS[CODE_ENV] || URLS.production;
 
 const App = () => {
   return (
