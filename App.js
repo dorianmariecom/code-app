4999b3e10934afcad45520f68c9d749e32807797
Dorian Marié <dorian@dorianmarie.com>
2024-07-29 21:12:10 +0200

    add react native device info for version and build number

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
-import React, {useRef} from 'react';
 import Config from 'react-native-config';
+import {getVersion, getBuildNumber} from 'react-native-device-info';
+import React, {useRef, useState} from 'react';
+import {NativeModules} from 'react-native';
 import {WebView} from 'react-native-webview';
-
+const {StatusBarManager} = NativeModules;
 const CODE_ENV = Config.CODE_ENV || 'production';
 const URLS = {
   local: 'http://localhost:3000',
@@ -10,16 +12,39 @@ const URLS = {
   production: 'https://codedorian.com',
 };
 const CODE_URL = Config.CODE_URL || URLS[CODE_ENV] || URLS.production;
+const VERSION = getVersion();
+const BUILD_NUMBER = getBuildNumber();
 
 const App = () => {
   const webViewRef = useRef();
+  const [statusBarHeight, setStatusBarHeight] = useState(0);
 
-  const onLoad = () => {
-    webViewRef.current.postMessage(JSON.stringify({config: Config}));
+  const update = () => {
+    postJSON({
+      buildNumber: BUILD_NUMBER,
+      config: Config,
+      statusBarHeight,
+      version: VERSION
+    });
   };
 
+  const postJSON = data => {
+    webViewRef.current.postMessage(JSON.stringify(data));
+  };
+
+  StatusBarManager.getHeight(statusBarHeight => {
+    setStatusBarHeight(statusBarHeight.height);
+    update();
+  });
+
   return (
-    <WebView ref={webViewRef} source={{uri: CODE_URL}} onLoad={onLoad} />
+    <WebView
+      ref={webViewRef}
+      source={{uri: CODE_URL}}
+      onLoadStart={update}
+      onLoad={update}
+      onLoadEnd={update}
+    />
   );
 };
 
