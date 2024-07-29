e904871669e3cdc43a6d0ceddb94256818bb2727
Dorian Marié <dorian@dorianmarie.com>
2024-07-29 17:32:13 +0200

    upgrades

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
-import React, { useRef } from 'react';
+import React, {useRef} from 'react';
 import Config from 'react-native-config';
-import { WebView } from 'react-native-webview';
+import {WebView} from 'react-native-webview';
 
-const CODE_ENV = Config.CODE_ENV || "production";
+const CODE_ENV = Config.CODE_ENV || 'production';
 const URLS = {
-  local: "http://localhost:3000",
-  dev: "https://dev.codedorian.com",
-  staging: "https://staging.codedorian.com",
-  production: "https://codedorian.com"
+  local: 'http://localhost:3000',
+  dev: 'https://dev.codedorian.com',
+  staging: 'https://staging.codedorian.com',
+  production: 'https://codedorian.com',
 };
 const CODE_URL = Config.CODE_URL || URLS[CODE_ENV] || URLS.production;
 
@@ -15,10 +15,12 @@ const App = () => {
   const webViewRef = useRef();
 
   const onLoad = () => {
-    webViewRef.current.postMessage(JSON.stringify({ config: Config }));
+    webViewRef.current.postMessage(JSON.stringify({config: Config}));
   };
 
-  return <WebView ref={webViewRef} source={{ uri: CODE_URL }} onLoadStart={alert} />;
+  return (
+    <WebView ref={webViewRef} source={{uri: CODE_URL}} onLoadStart={alert} />
+  );
 };
 
 export default App;
